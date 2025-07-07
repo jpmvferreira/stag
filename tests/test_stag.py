b65aa98d3fe1556f575c90b6b0790fec60a67c5b
JPUnD <jose@jpferreira.me>
2025-07-07 22:49:05 +0100

    implemented first runnable test

--- tests/test_stag.py
+++ tests/test_stag.py
@@ -1,7 +1,10 @@
 # global imports
+from fuse import FUSE, FuseOSError, Operations
+import multiprocessing
 import tempfile
 import sqlite3
 import shutil
+import time
 import os
 
 # local imports
@@ -12,8 +15,8 @@ class TestStag:
     # Auxiliary functions
     # ---------------------------------------------------------------------------------------------------------------
 
-    # setup a stag repository for each test
-    def __setup_stag(self):
+    # create a stag repository, should be used before each test
+    def __create_stag(self):
         # create directories
         tmpdir = tempfile.mkdtemp()
         src = f"{tmpdir}/src"
@@ -25,13 +28,18 @@ class TestStag:
         os.mkdir(files)
 
         # create dummy files
-        with open(f"{files}/berne.txt",     "w") as f: f.write("berne.txt")
-        with open(f"{files}/castle.txt",    "w") as f: f.write("castle.txt")
-        with open(f"{files}/fuji.txt",      "w") as f: f.write("fuji.txt")
-        with open(f"{files}/mountains.txt", "w") as f: f.write("mountains.txt")
-        with open(f"{files}/porto.txt",     "w") as f: f.write("porto.txt")
-        with open(f"{files}/tallinn.txt",   "w") as f: f.write("tallinn.txt")
-        with open(f"{files}/shirakawa.txt", "w") as f: f.write("shirakawa.txt")
+        with open(f"{files}/berne.txt",     "w") as f: f.write("content of berne.txt")
+        with open(f"{files}/castle.txt",    "w") as f: f.write("content of castle.txt")
+        with open(f"{files}/fuji.txt",      "w") as f: f.write("content of fuji.txt")
+        with open(f"{files}/mountains.txt", "w") as f: f.write("content of mountains.txt")
+        with open(f"{files}/porto.txt",     "w") as f: f.write("content of porto.txt")
+        with open(f"{files}/tallinn.txt",   "w") as f: f.write("content of tallinn.txt")
+        with open(f"{files}/shirakawa.txt", "w") as f: f.write("content of shirakawa.txt")
+
+        # create directories (tags)
+        os.mkdir(f"{files}/city")
+        os.mkdir(f"{files}/mountains")
+        os.mkdir(f"{files}/snow")
 
         # create database
         db  = f"{src}/stag.sql"
@@ -41,13 +49,13 @@ class TestStag:
         cur.execute("CREATE TABLE files(path, tags)")
         cur.execute("""
             INSERT INTO files VALUES
-                ('berne.jpg',     '/city//mountains/'      ),
-                ('castle.jpg',    '/snow//mountains/'      ),
-                ('fuji.jpg',      '/city//mountains/'      ),
-                ('mountains.jpg', '/mountains/'            ),
-                ('porto.jpg',     '/city/'                 ),
-                ('tallinn.jpg',   '/city//snow/'           ),
-                ('shirakawa.jpg', '/city//snow//mountains/')
+                ('berne.txt',     '/city//mountains/'      ),
+                ('castle.txt',    '/snow//mountains/'      ),
+                ('fuji.txt',      '/city//mountains/'      ),
+                ('mountains.txt', '/mountains/'            ),
+                ('porto.txt',     '/city/'                 ),
+                ('tallinn.txt',   '/city//snow/'           ),
+                ('shirakawa.txt', '/city//snow//mountains/')
         """)
 
         con.commit()
@@ -56,10 +64,25 @@ class TestStag:
         # create filesystem object
         fs = stag.StagFS(files, db)
 
-        return fs, tmpdir
+        return fs, tmpdir, mnt
 
-    # cleanup the mock stag repository after running each test
-    def __clean_stag(self, tmpdir):
+    # mount a stag repository
+    def __mount_stag(self, fs, mnt):
+        p = multiprocessing.Process(target=lambda mnt: FUSE(fs, mnt, nothreads=True, foreground=True), args=(mnt,))
+        p.start()
+        time.sleep(0.1)  # not sure I need this or previous operation is blocking
+
+        return p
+
+    # unmount a stag repository, must be called before __remove_stag if there was acall to __mount_stag
+    def __unmount_stag(self, p):
+        p.terminate()
+        p.join()
+
+        pass
+
+    # remove a stag repository, should be used after running each test
+    def __remove_stag(self, tmpdir):
         shutil.rmtree(tmpdir)
 
         pass
@@ -68,53 +91,70 @@ class TestStag:
     # Tests on filesystem methods
     # ---------------------------------------------------------------------------------------------------------------
 
-    def test_access(self):
-        fs, tmpdir = self.__setup_stag()
-        self.__clean_stag(tmpdir)
+    #def test_access(self):
+    #    pass
 
-        pass
+    #def test_chmod(self):
+    #    pass
 
-    def test_chmod(self):
-        pass
+    #def test_chown(self):
+    #    pass
 
-    def test_chown(self):
-        pass
+    #def test_getattr(self):
+    #    pass
 
-    def test_getattr(self):
-        pass
+    #def test_rename(self):
+    #    pass
 
-    def test_rename(self):
-        pass
+    #def test_statfs(self):
+    #    pass
 
-    def test_statfs(self):
-        pass
+    #def test_symlink(self):
+    #    pass
 
-    def test_symlink(self):
-        pass
+    #def test_unlink(self):
+    #    pass
 
-    def test_unlink(self):
-        pass
-
-    def test_utimens(self):
-        pass
+    #def test_utimens(self):
+    #    pass
 
     # ---------------------------------------------------------------------------------------------------------------
     # Tests on folder methods
     # ---------------------------------------------------------------------------------------------------------------
 
-    def test_mkdir(self):
-        pass
+    #def test_mkdir(self):
+    #    pass
 
-    def test_readdir(self):
-        pass
+    #def test_readdir(self):
+    #    pass
 
-    def test_rmdir(self):
-        pass
+    #def test_rmdir(self):
+    #    pass
 
     # ---------------------------------------------------------------------------------------------------------------
     # Tests on file methods
     # ---------------------------------------------------------------------------------------------------------------
 
-    # criar um test que testa varias funcionalidades nao so uma, e.g.: criar e escrever, ou abrir e escrever, etc.
-    def test_file1(self):
+    #def test_file1(self):
+    #    pass
+
+    # ---------------------------------------------------------------------------------------------------------------
+    # Tests using terminal utilities
+    # ---------------------------------------------------------------------------------------------------------------
+
+    def test_cli1(self):
+        fs, tmpdir, mnt = self.__create_stag()
+        p = self.__mount_stag(fs, mnt)
+
+        ls = os.popen(f"ls {mnt}").read()
+        ls_city = os.popen(f"ls {mnt}/city").read()
+
+        try:
+            assert ls == "berne.txt\ncastle.txt\ncity\nfuji.txt\nmountains\nmountains.txt\nporto.txt\nshirakawa.txt\nsnow\ntallinn.txt\n"
+            assert ls_city == "berne.txt\nfuji.txt\nmountains\nporto.txt\nshirakawa.txt\nsnow\ntallinn.txt\n"
+
+        finally:
+            self.__unmount_stag(p)
+            self.__remove_stag(tmpdir)
+
         pass
