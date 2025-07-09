93ee2415ac5a7d6724c4decfab7806443ba7c80e
JPUnD <jose@jpferreira.me>
2025-07-09 22:52:42 +0100

    implemented more complete suite of tests

--- tests/test_stag.py
+++ tests/test_stag.py
@@ -88,61 +88,11 @@ class TestStag:
         pass
 
     # ---------------------------------------------------------------------------------------------------------------
-    # Tests on filesystem methods
+    # Tests
     # ---------------------------------------------------------------------------------------------------------------
 
-    #def test_access(self):
-    #    pass
-
-    #def test_chmod(self):
-    #    pass
-
-    #def test_chown(self):
-    #    pass
-
-    #def test_getattr(self):
-    #    pass
-
-    #def test_rename(self):
-    #    pass
-
-    #def test_statfs(self):
-    #    pass
-
-    #def test_symlink(self):
-    #    pass
-
-    #def test_unlink(self):
-    #    pass
-
-    #def test_utimens(self):
-    #    pass
-
-    # ---------------------------------------------------------------------------------------------------------------
-    # Tests on folder methods
-    # ---------------------------------------------------------------------------------------------------------------
-
-    #def test_mkdir(self):
-    #    pass
-
-    #def test_readdir(self):
-    #    pass
-
-    #def test_rmdir(self):
-    #    pass
-
-    # ---------------------------------------------------------------------------------------------------------------
-    # Tests on file methods
-    # ---------------------------------------------------------------------------------------------------------------
-
-    #def test_file1(self):
-    #    pass
-
-    # ---------------------------------------------------------------------------------------------------------------
-    # Tests using terminal utilities
-    # ---------------------------------------------------------------------------------------------------------------
-
-    def test_cli1(self):
+    # list files and folders on the root directory and in a random subfolder
+    def test_ls(self):
         fs, tmpdir, mnt = self.__create_stag()
         p = self.__mount_stag(fs, mnt)
 
@@ -152,6 +102,115 @@ class TestStag:
         try:
             assert ls == "berne.txt\ncastle.txt\ncity\nfuji.txt\nmountains\nmountains.txt\nporto.txt\nshirakawa.txt\nsnow\ntallinn.txt\n"
             assert ls_city == "berne.txt\nfuji.txt\nmountains\nporto.txt\nshirakawa.txt\nsnow\ntallinn.txt\n"
+
+        finally:
+            self.__unmount_stag(p)
+            self.__remove_stag(tmpdir)
+
+        pass
+
+    # create a directory (i.e. a tag)
+    def test_mkdir(self):
+        fs, tmpdir, mnt = self.__create_stag()
+        p = self.__mount_stag(fs, mnt)
+
+        os.system(f"mkdir {mnt}/buildings")
+        ls = os.popen(f"ls {mnt}").read()
+
+        try:
+            assert "buildings" in ls
+
+        finally:
+            self.__unmount_stag(p)
+            self.__remove_stag(tmpdir)
+
+        pass
+
+    # remove a file from the repository
+    def test_rm(self):
+        fs, tmpdir, mnt = self.__create_stag()
+        p = self.__mount_stag(fs, mnt)
+
+        os.system(f"rm {mnt}/fuji.txt")
+        ls = os.popen(f"ls {mnt}").read()
+        ls_city = os.popen(f"ls {mnt}/city").read()
+
+        try:
+            assert "fuji.txt" not in ls
+            assert "fuji.txt" not in ls_city
+
+        finally:
+            self.__unmount_stag(p)
+            self.__remove_stag(tmpdir)
+
+        pass
+
+    # remove tag(s) from file(s)
+    def test_rm2(self):
+
+        # TODO: remove some tags from a file and the check subfolders to see if its gone but still exists in the root repo
+
+        pass
+
+    # remove tag(s) from the repository
+    def test_rmdir(self):
+
+        # TODO: remove a tag or two from the directory, check root folder and subfolders
+
+        pass
+
+    # read content from file(s)
+    def test_read(self):
+        fs, tmpdir, mnt = self.__create_stag()
+        p = self.__mount_stag(fs, mnt)
+
+        ls = os.popen(f"ls {mnt}").read()
+        ls_mountains = os.popen(f"ls {mnt}/mountains").read()
+
+        try:
+            for path in ls.split():
+                if ".txt" in path:
+                    assert f"content of {path}" == os.popen(f"cat {mnt}/{path}").read()
+
+            for path in ls_mountains.split():
+                if ".txt" in path:
+                    assert f"content of {path}" == os.popen(f"cat {mnt}/mountains/{path}").read()
+
+        finally:
+            self.__unmount_stag(p)
+            self.__remove_stag(tmpdir)
+
+        pass
+
+    # write new contents to an already existing file
+    def test_write(self):
+        fs, tmpdir, mnt = self.__create_stag()
+        p = self.__mount_stag(fs, mnt)
+
+        os.system(f"echo -n 'something totally different' > {mnt}/porto.txt")
+
+        try:
+            assert os.popen(f"cat {mnt}/porto.txt").read() == "something totally different"
+
+        finally:
+            self.__unmount_stag(p)
+            self.__remove_stag(tmpdir)
+
+        pass
+
+    # create a new file on the repository
+    def test_create(self):
+        fs, tmpdir, mnt = self.__create_stag()
+        p = self.__mount_stag(fs, mnt)
+
+        os.system(f"echo -n 'content of lisbon.txt' > {mnt}/city/lisbon.txt")
+        ls = os.popen(f"ls {mnt}").read()
+        ls_city = os.popen(f"ls {mnt}/city").read()
+
+        try:
+            assert "lisbon.txt" in ls
+            assert "lisbon.txt" in ls_city
+            assert "content of lisbon.txt" == os.popen(f"cat {mnt}/lisbon.txt").read()
 
         finally:
             self.__unmount_stag(p)
